e0ed493ef655ca3a1f3dfe871d983edb3ac01eaa
gjl5108 <3120005108@mail2.gdut.edu.cn>
2022-07-15 14:05:26 +0800

    打印22222

--- src/main/java/com/gjl/git/GitTest.java
+++ src/main/java/com/gjl/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("11111");
+        System.out.println("222222");
     }
 }
