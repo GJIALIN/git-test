77500815d56abd7d9cf73c7ff5ac17b88b07fe55
gjl5108 <3120005108@mail2.gdut.edu.cn>
2022-07-15 14:22:51 +0800

    删除

--- src/main/java/com/gjl/git/GitTest.java
+++ src/main/java/com/gjl/git/GitTest.java
@@ -2,20 +2,7 @@ package com.gjl.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
-        System.out.println("pull test");
-        System.out.println("11111");
-        System.out.println("222222");
-        System.out.println("333");
-        System.out.println("444");
-        System.out.println("5");
+        System.out.println("删除123");
 
     }
 }
